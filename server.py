aec344525c6a2c4c54e61e0885b691a07aca1aa4
Hackbright Student <no-reply@hackbrightacademy.com>
2019-08-06 16:59:04 -0700

    Made login form and made change to registration form, also added new routes for login.

--- server.py
+++ server.py
@@ -24,12 +24,62 @@ def index():
     """Homepage."""
     return render_template("homepage.html")
 
+
+@app.route('/register', methods=["GET"])
+def register_form():
+    """Displays registration form"""
+
+    return render_template("registration_form.html")
+
+
+@app.route('/register', methods=["POST"])
+def register_process():
+    """Processes registration form"""
+
+    # Grab data from form
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+
+    if  User.query.filter(User.email == email).first().email:
+       # User.query(User.email).filter(User.email == email).first() 
+       flash("Email already exists!")
+       return redirect('/register')
+    else:
+        # Adds and commits user's email and password into the database 
+        db.session.add(User(email=email, password=password))
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/login', methods=["GET"])
+def login_form():
+    """Displays login form"""
+
+    return render_template("login_form.html")
+
+
+@app.route('/login', methods =["POST"])
+def login_process():
+    """Redirects user to homepage after login message"""
+
+
+    # query for that email address in database
+        # if email matches the password 
+            #log user in - adding user id - from db to flask session
+            # flash("Logged in")
+            # redirect back to homepage
+        # else error message 
+            # redirect back to login page?
+            # hyperlink registration page
+    return redirect("/")
+
 @app.route('/users')
 def user_list():
     """Show list of users."""
 
     users = User.query.all()
     return render_template("user_list.html", users=users)
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
