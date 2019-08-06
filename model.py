1238525d7ca15fb91faf9129e0077f418b855653
Hackbright Student <no-reply@hackbrightacademy.com>
2019-08-06 12:42:44 -0700

    Added relationships to ratings

--- model.py
+++ model.py
@@ -48,9 +48,24 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    
+    movie_id = db.Column(db.Integer, 
+                         db.ForeignKey('movies.movie_id'),
+                         nullable=False)
+    
+    user_id = db.Column(db.Integer, 
+                        db.ForeignKey('users.user_id'),
+                        nullable=False)
+    
     score = db.Column(db.Integer, nullable=False)
+
+    # Define relationship to user
+    user = db.relationship("User", 
+                           backref=db.backref("ratings", order_by=rating_id))
+    
+    # Define relation to movie
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings", order_by=rating_id))
 
 ##############################################################################
 # Helper functions
